00130b376d1fa7f84bf5a5fd54bec1bf3565c552
quathan <mlfiloramo@gmail.com>
2021-04-11 16:43:05 -0400

    My first commit

---
+++ main/config.py
@@ -0,0 +1,6 @@
+client_id = "whlMP0C9rsqDsg"
+client_secret = "UXgHQPC4fUYPcaHCz3MgkByt0G4lvg"
+password = "Quailk777!!!"
+username = "test_bot_xena"
+user_agent = "test_bot_xena"
+
---
+++ main/reddit_bot.py
@@ -0,0 +1,53 @@
+import praw
+
+import config
+
+
+def login():
+    r = praw.Reddit(user_agent=config.user_agent,
+                    username=config.username,
+                    password=config.password,
+                    client_id=config.client_id,
+                    client_secret=config.client_secret,
+                    )
+    print("Logged in.")
+    return r
+
+
+def run_bot(r):
+    """Scrapes a subreddit and continually lists the top 10 posts."""
+    submission = r.submission(url="https://www.reddit.com/user/test_bot_xena/comments/mm4oqz/im_a_bot_testing/")
+    all_comments = submission.comments.list()
+    logged_posts = []
+    with open('log.txt', 'r+') as log_file:
+        opened_file = log_file.readlines()
+        for line in opened_file:
+            logged_posts.append(line.rstrip())
+        for comment in all_comments:
+            if comment.author == "Calif0rnia_Soul" and comment not in logged_posts:
+                log_file.write(f"{comment}\n")
+                print("New comment detected. Responding...")
+                comment.reply("Hello there!")
+            # else:
+            #     continue
+
+
+def delete_comments(r):
+    """Deletes all the bot's comments from itself."""
+    submission = r.submission(id="mm4oqz")
+    all_comments = submission.comments.list()
+    for comment in all_comments:
+        if comment.author == "test_bot_xena":
+            print("Deleting comments...")
+            comment.delete()
+
+
+reddit = login()
+
+print("Running...")
+
+while True:
+    run_bot(reddit)
+    # delete_comments(reddit)
+
+
