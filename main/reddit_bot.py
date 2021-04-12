e61200a5374b3e0315de7d9ee1f7a16a06a229e9
quathan <mlfiloramo@gmail.com>
2021-04-12 10:45:28 -0400

    Cleaned up unused code from rand_sentences. Also removed the module.

--- main/reddit_bot.py
+++ main/reddit_bot.py
@@ -1,6 +1,5 @@
 import praw
 from main import config
-from modules import random_sentences_gen
 from modules import rand_items
 import random
 
@@ -20,10 +19,12 @@ def run_bot(r):
     """Looks at comments within own submission and automatically responds to user."""
     submission = r.submission(url="https://www.reddit.com/user/test_bot_xena/comments/mm4oqz/im_a_bot_testing/")
     all_comments = submission.comments.list()
+
     # with open('log.txt', 'r+') as log_file:
     #     opened_file = log_file.readlines()
     #     for line in opened_file:
     #         logged_posts.append(line.rstrip())
+
     for comment in all_comments:
         word_count = 0
         word_limit = random.randint(4, 12)
@@ -33,14 +34,10 @@ def run_bot(r):
             sentence += random.choice([random.choice(rand_items.conjunctions).lower(),
                                        random.choice(rand_items.words)]).lower() + str(" ")
             word_count += 1
-
         if word_count == word_limit:
             sentence += random.choice([random.choice(rand_items.conjunctions).lower(),
                                        random.choice(rand_items.words)]).lower()
 
-        # random_sentence = f"{random_sentences_gen.sentence.rstrip(' ').capitalize()}"
-        #                    f"{random.choice(rand_items.punctuation)}"
-        # if comment.author == "Calif0rnia_Soul" and comment not in logged_posts:
         if "!summon" in comment.body and comment not in cache:
             # log_file.write(f"{comment}\n")
             cache.append(f"{comment}")
@@ -67,7 +64,7 @@ reddit = login()
 print("Running...")
 
 while True:
-    run_bot(reddit)
-    # delete_comments(reddit)
+    # run_bot(reddit)
+    delete_comments(reddit)
 
 
