803ecd63c10a2cae35371b93f5e3219cf6e44305
quathan <mlfiloramo@gmail.com>
2021-04-12 09:31:22 -0400

    Changed log_file global variable and omitted log.txt dependency.

--- main/reddit_bot.py
+++ main/reddit_bot.py
@@ -17,16 +17,15 @@ def run_bot(r):
     """Looks at comments within own submission and automatically responds to user."""
     submission = r.submission(url="https://www.reddit.com/user/test_bot_xena/comments/mm4oqz/im_a_bot_testing/")
     all_comments = submission.comments.list()
-    logged_posts = []
     # with open('log.txt', 'r+') as log_file:
     #     opened_file = log_file.readlines()
     #     for line in opened_file:
     #         logged_posts.append(line.rstrip())
     for comment in all_comments:
         # if comment.author == "Calif0rnia_Soul" and comment not in logged_posts:
-        if "!summon" in comment.body and comment not in logged_posts:
+        if "!summon" in comment.body and comment not in cache:
             # log_file.write(f"{comment}\n")
-            logged_posts.append(f"{comment}\n")
+            cache.append(f"{comment}")
             print("New comment detected. Responding...")
             comment.reply("Beep boop. I'm a bot in the making!\n\n"
                           "*This action was performed automatically.*")
@@ -41,6 +40,7 @@ def delete_comments(r):
             print("Deleting comments...")
             comment.delete()
 
+cache = []
 
 reddit = login()
 
