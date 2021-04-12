8ef6c5e696387d0aac903ea9990d577f883d4f4f
quathan <mlfiloramo@gmail.com>
2021-04-12 08:56:14 -0400

    Modified comment detection conditions.

--- main/reddit_bot.py
+++ main/reddit_bot.py
@@ -18,14 +18,15 @@ def run_bot(r):
     submission = r.submission(url="https://www.reddit.com/user/test_bot_xena/comments/mm4oqz/im_a_bot_testing/")
     all_comments = submission.comments.list()
     logged_posts = []
-    with open('log.txt', 'r+') as log_file:
-        opened_file = log_file.readlines()
-        for line in opened_file:
-            logged_posts.append(line.rstrip())
+    # with open('log.txt', 'r+') as log_file:
+    #     opened_file = log_file.readlines()
+    #     for line in opened_file:
+    #         logged_posts.append(line.rstrip())
         for comment in all_comments:
             # if comment.author == "Calif0rnia_Soul" and comment not in logged_posts:
             if "!summon" in comment.body and comment not in logged_posts:
-                log_file.write(f"{comment}\n")
+                # log_file.write(f"{comment}\n")
+                logged_posts.append(f"{comment}\n")
                 print("New comment detected. Responding...")
                 comment.reply("Beep boop. I'm a bot in the making!\n\n"
                               "*This action was performed automatically.*")
