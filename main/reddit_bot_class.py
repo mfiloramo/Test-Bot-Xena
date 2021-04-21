8baf282b5dd7abfc5d1ebe25d78500f5c123dc3f
quathan <mlfiloramo@gmail.com>
2021-04-20 20:25:17 -0400

    Cleaned up code in reddit_bot_class.

--- main/reddit_bot_class.py
+++ main/reddit_bot_class.py
@@ -5,28 +5,20 @@ import config
 class RedditBot:
     """An instance of a Reddit bot."""
 
-    def __init__(self, user_agent, username, password, client_id, client_secret):
-        """Initialize an instance of a Reddit bot with correct login credentials."""
-        self.user_agent = user_agent
-        self.username = username
-        self.password = password
-        self.client_id = client_id
-        self.client_secret = client_secret
-
-    def login(self):
-        """Logs into Reddit via PRAW."""
-        reddit = praw.Reddit(user_agent=self.user_agent,
-                             username=self.username,
-                             password=self.password,
-                             client_id=self.client_id,
-                             client_secret=self.client_secret,
-                             )
-        print("Logged in.")
-        return reddit
+    def __init__(self):
+        """Initialize an instance of a Reddit bot and log in with agent/user credentials."""
+        self.login = \
+            praw.Reddit(user_agent=config.user_agent,
+                        username=config.username,
+                        password=config.password,
+                        client_id=config.client_id,
+                        client_secret=config.client_secret,
+                        )
+        print('Logged in.')
 
     def scrape_subreddit(self):
         """Scrapes and prints all comments and comment replies in all posts within a subreddit."""
-        subreddit = self.login().subreddit('learnpython')
+        subreddit = self.login.subreddit('learnpython')
         print('Running...')
         while True:
             for submission in subreddit.new(limit=10):
@@ -37,8 +29,6 @@ class RedditBot:
                         print(f"\t{reply.body}\n")
 
 
-run_bot = RedditBot(config.user_agent, config.username, config.password,
-                    config.client_id, config.client_secret)
 
 if __name__ == '__main__':
-    run_bot.scrape_subreddit()
+    RedditBot().scrape_subreddit()
